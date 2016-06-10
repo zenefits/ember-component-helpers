55aa7151fa5d2af668f6c73daa57aca7f7d68b51
Miguel Madero <git@miguelmadero.com>
2016-06-10 10:18:38 -0300

    Adds an scenario to show that the properties in the hash aren’t bound

--- tests/dummy/app/controllers/append.js
+++ tests/dummy/app/controllers/append.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  ctrlHash: {'class':'x-append'}
+  ctrlHash: {'class':'x-append'},
+  actions: {
+    changeHashValue () {
+      this.set('ctrlHash.class', 'new-class');
+    }
+  }
 });
